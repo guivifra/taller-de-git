53a141b4124d180ad26cb33aa7e192a85ce76c7c
Guillermo Vidal <g.vidalfrasquet@edu.gva.es>
2023-12-11 12:32:32 +0100

    Mogut hola.php a lib/

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php
-// Autor: Guillermo Vidal
+// Autor: Guillermo Vidal <g.vidalfrasquet@edu.gva.es>
 // El nom per defecte és Món
 $nombre = isset($argv[1]) ? $argv[1] : "Món";
 @print "Hola {$nombre}\n";
